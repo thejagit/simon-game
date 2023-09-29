b05416bc32443e828fcef44ff19bcd00a63401ca
Thejani Kulathunga <thejani.kulathunga@gmail.com>
2023-09-29 23:11:57 +0530

    first commit-adding an animation

--- js/index.js
+++ js/index.js
@@ -1,72 +1,74 @@
-let gameLevel=0;
-let started=0;
-const buttonColors = ["green", "red", "yellow","blue"];
-$(document).ready(function(){
-// press any key to start the game
-$(document).keypress(function(e){
-    if(!started){
-        gameLevel++;
-        started = true;
+let gameLevel = 0;
+let started = 0;
+const buttonColors = ["green", "red", "yellow", "blue"];
+$(document).ready(function () {
+    // press any key to start the game
+    $(document).keypress(function (e) {
+        if (!started) {
+            gameLevel++;
+            started = true;
 
-        emptyHeading();
-        changeLevel(gameLevel);
-    }
+            emptyHeading();
+            changeLevel(gameLevel);
+        }
+    });
+
+
+    $(".btn").click(function () {
+        let btnId = $(this).attr("id");
+        playGame(btnId);
+
+    });
+
 
 });
 
+// changing headings
+function emptyHeading() {
+    $("h1").empty();
+}
 
-
-$(".btn").click(function(){
-   let btnId=$(this).attr("id");
-   let userSelectedId=buttonColors.indexOf(btnId);
-   let randomId=randomNumber();
+function changeLevel(gameLevel) {
+    $("h1").append(" Level " + gameLevel);
+}
+function playGame(btnId) {
+    let userSelectedId = buttonColors.indexOf(btnId);
+    let randomId = randomNumber();
+    $("#"+btnId).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     // alert("hi "+userSelectedId+"\n Random Number -"+randomId);
-     if(userSelectedId===randomId){
-       // alert("hi I'm ramdom num "+randomId);
+    if (userSelectedId === randomId) {
+        // alert("hi I'm ramdom num "+randomId);
         gameLevel++;
         emptyHeading();
         changeLevel(gameLevel);
         playMusic(btnId);
-    }else{
+    } else {
         emptyHeading();
         gameOver();
         musicWrong();
-     
     }
-
-});
-
-
-});
-// changing headings
-function emptyHeading(){
-     $("h1").empty();
-}
-
-function changeLevel(gameLevel){
-     $("h1").append(" Level "+gameLevel); 
 }
 // callback function
-function gameOver(){
-  $("h1").append(" Game Over, Press Any Key to Restart");
-  gameLevel=0; 
-started = false;  
+function gameOver() {
+    $("h1").append(" Game Over, Press Any Key to Restart");
+    gameLevel = 0;
+    started = false;
 }
 
 // adding music sounds
-function playMusic(name){
-    var audioMusic= new Audio("../sounds/"+name+".mp3");
+function playMusic(name) {
+    var audioMusic = new Audio("../sounds/" + name + ".mp3");
     audioMusic.play();
 }
-function musicWrong(){
-    var audioWrong= new Audio("../sounds/wrong.mp3");
+function musicWrong() {
+    var audioWrong = new Audio("../sounds/wrong.mp3");
     audioWrong.play();
 }
 
 //generate random number
-function  randomNumber(){
-// Returns a random integer from 0 to 3:
-let randomNum=Math.floor(Math.random() * 4);
-return randomNum;
+function randomNumber() {
+    // Returns a random integer from 0 to 3:
+    let randomNum = Math.floor(Math.random() * 4);
+    return randomNum;
 
 }
